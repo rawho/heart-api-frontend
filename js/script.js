05c5a4502686943d24715236af7eee38a4be8da8
Rahul T <rahulmanoj@cet.ac.in>
2020-11-06 23:01:06 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+const form = document.querySelector("form")
+const container = document.querySelector(".container")
+const resultpositive = document.getElementById("result-positive")
+const resultnegative = document.getElementById("result-negative")
+resultpositive.style.display = "none"
+resultnegative.style.display = "none"
+
+const errorMsg = document.querySelector('.msg')
+errorMsg.style.display = 'none'
+
+const spinner = document.querySelector('.loader-container')
+spinner.style.display = 'none'
+
+
+form.addEventListener('submit', async (e) => {
+    
+    spinner.style.display = ''
+    e.preventDefault()
+    const formdata = new FormData(form).entries()
+    let res = await fetch('https://heartmlapi.herokuapp.com/weight/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formdata))
+    })
+    if (res.status == 500) {
+        spinner.style.display = 'none'
+        errorMsg.style.display = ''
+        setTimeout(() => {
+            errorMsg.style.display = 'none'
+        }, 5000);
+    }
+
+    spinner.style.display - ''
+
+    let json = await res.json()
+    console.log(json)
+    container.style.display = 'none'
+
+    if (json == 1) {
+        spinner.style.display = 'none'
+        resultpositive.style.display = ''
+    }
+    else if (json == 0) {
+        spinner.style.display = 'none'
+        resultnegative.style.display = ''
+    }
+
+})
